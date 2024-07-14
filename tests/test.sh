7244d93ddfd69ba1a91100c72b330902cbebdf5a
Benjamin Eckel <bhelx@simst.im>
2024-07-14 09:27:38 -0500

    whitespace, fix ghaction

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,11 @@
 set -e
 
+# Make sure template is built
 cd ../
 ./bundle.sh
 cd tests
 
-
-
+# Run through every schema in schemas/
 for file in ./schemas/*.yaml; do
   echo "Generating and testing $file..."
   rm -rf output

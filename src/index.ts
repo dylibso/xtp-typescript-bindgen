ae626de8b8c6b0c45697a330ec1bb52befe22fda
Muhammad Azeez <muhammad@dylibso.com>
2024-10-22 19:02:22 +0300

    map properties

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import ejs from 'ejs'
-import { helpers, getContext, Property, Parameter } from "@dylibso/xtp-bindgen"
+import { helpers, getContext, Property, Parameter,  } from "@dylibso/xtp-bindgen"
 
 function toTypeScriptType(property: Property | Parameter): string {
   let tp
   if (property.$ref) {
     tp = property.$ref.name
+  } else if (property.additionalProperties) {
+    tp = `Record<string, ${toTypeScriptType(property.additionalProperties)}>`
   } else {
     switch (property.type) {
       case "string":
@@ -49,6 +51,81 @@ function toTypeScriptType(property: Property | Parameter): string {
   return `${tp} | null`
 }
 
+function getBaseRef(property: Property): string {
+  if (property.additionalProperties) {
+    if (property.additionalProperties.items?.$ref) {
+      return property.additionalProperties.items.$ref.name
+    }
+
+    return property.additionalProperties.$ref?.name || ''
+  }
+
+  if (property.items) {
+    return property.items.$ref?.name || ''
+  }
+
+  return property.$ref?.name || ''
+}
+
+function getPropertyValue(prop: Property): string | null {
+  const baseP = (prop.items ? prop.items : prop) as Property;
+  const baseRef = getBaseRef(prop);
+  
+  if (helpers.isDateTime(baseP)) {
+    return `cast(dateFromJson, obj.${prop.name})`;
+  } else if (isBuffer(baseP)) {
+    return `cast(bufferFromJson, obj.${prop.name})`;
+  } else if (helpers.isMap(prop)) {
+    return getMapConverter(prop);
+  } else if (!helpers.isPrimitive(baseP)) {
+    return `cast(${baseRef}.fromJson, obj.${prop.name})`;
+  }
+  return null;
+}
+
+// Helper function to handle map conversion
+function getMapConverter(prop: Property): string {
+  if (helpers.isMap(prop.additionalProperties as Property)) {
+    // Handle nested maps recursively
+    const innerConverter = getPropertyValue(prop.additionalProperties as Property);
+    return `mapFromJson(obj.${prop.name}, (v) => ${innerConverter})`;
+  } else if (!helpers.isPrimitive(prop.additionalProperties as Property)) {
+    const baseRef = getBaseRef(prop.additionalProperties as Property);
+    return `mapFromJson(obj.${prop.name}, ${baseRef}.fromJson)`;
+  }
+
+  return `mapFromJson(obj.${prop.name}, (v: any) => v as ${toTypeScriptType(prop.additionalProperties as Property)})`;
+}
+
+function getPropertyToJsonValue(prop: Property): string | null {
+  const baseP = (prop.items ? prop.items : prop) as Property;
+  const baseRef = getBaseRef(prop);
+  
+  if (helpers.isDateTime(baseP)) {
+    return `cast(dateToJson, obj.${prop.name})`;
+  } else if (isBuffer(baseP)) {
+    return `cast(bufferToJson, obj.${prop.name})`;
+  } else if (helpers.isMap(prop)) {
+    return getMapToJsonConverter(prop);
+  } else if (!helpers.isPrimitive(baseP)) {
+    return `cast(${baseRef}.toJson, obj.${prop.name})`;
+  }
+  return null;
+}
+
+function getMapToJsonConverter(prop: Property): string {
+  if (helpers.isMap(prop.additionalProperties as Property)) {
+    // Handle nested maps recursively
+    const innerConverter = getPropertyToJsonValue(prop.additionalProperties as Property);
+    return `mapToJson(obj.${prop.name}, (v) => ${innerConverter})`;
+  } else if (!helpers.isPrimitive(prop.additionalProperties as Property)) {
+    const baseRef = getBaseRef(prop.additionalProperties as Property);
+    return `mapToJson(obj.${prop.name}, ${baseRef}.toJson)`;
+  }
+  return `obj.${prop.name}`;
+}
+
+
 // TODO: can move this helper up to shared library?
 function isBuffer(property: Property | Parameter): boolean {
   return property.type === 'buffer'
@@ -61,6 +138,9 @@ export function render() {
     ...helpers,
     isBuffer,
     toTypeScriptType,
+    getBaseRef,
+    getPropertyValue,
+    getPropertyToJsonValue
   }
   const output = ejs.render(tmpl, ctx)
   Host.outputString(output)
